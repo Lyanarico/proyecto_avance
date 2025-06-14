eaa345ff74efa12a02640c27c5f4f23e1dc8e42f
ShiroIshii0 <raul.morales.castroggg@gmail.com>
2025-06-14 18:17:04 -0400

    Delete proyecto_avance_css.txt/intercabio_libros directory

--- proyecto_avance_css.txt/intercabio_libros/historial/historial/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Historial
-
-@admin.register(Historial)
-class HistorialAdmin(admin.ModelAdmin):
-    list_display = ('usuario', 'accion', 'fecha', 'solicitud')
-    list_filter = ('accion', 'fecha', 'usuario')
-    search_fields = ('usuario__username', 'accion', 'solicitud__id')
--- proyecto_avance_css.txt/intercabio_libros/historial/historial/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'historial'
-
-urlpatterns = [
-    path('', views.listar_historial, name='listar_historial'),
-]
-
